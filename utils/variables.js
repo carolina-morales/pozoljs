49db1774e579da080ba847149eeb009969768a05
Daniel Morales <daniel.cmorales3@gmail.com>
2020-09-14 23:05:44 -0500

    Delete old JavaScript files

--- utils/variables.js
+++
@@ -1,3 +0,0 @@
-export const regex = /[áéíóúÁÉÍÓÚ\/*:.,\\;¿¡'?~`¨´{}\[\]^°¬|!"#$ñÑ%&()=<>_0-9]+/gm;
-export const integrations = [ 'TypeScript', 'JavaScript' ];
-export const types = [ 'component', 'controller', 'service', 'interface', 'routes' ];
