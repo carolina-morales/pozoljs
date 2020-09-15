940e60287d5870a3f5e62704bdb0f55496bb93f6
Daniel Morales <daniel.cmorales3@gmail.com>
2020-09-14 19:21:38 -0500

    UI folders to show elements in console

---
+++ src/ui/messages.ui.ts
@@ -0,0 +1,15 @@
+export const INFO_MESSAGES = {
+	PROJECT_INFORMATION_START: `We will scaffold your app in a few seconds...`,
+	FILES_INFORMATION_START: `We are generating your files...`,
+	GET_STARTED_INFORMATION: `Get started with the following commands:`
+};
+
+export const SUCCESS_MESSAGES = {
+	PACKAGE_MANAGER_INSTALLATION_SUCCEED: `Successfully created a new project`
+};
+
+export const ERROR_MESSAGES = {
+	RUNNER_EXECUTION_ERROR: `Failed to execute command`,
+	GIT_INITIALIZATION_ERROR: 'Git repository has not been initialized',
+	NPM_INSTALLING_ERROR: 'The packages could not be installed'
+};
