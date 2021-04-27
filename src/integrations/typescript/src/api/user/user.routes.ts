359a23f8990e3922b62d120d2b418ced31c96beb
Daniel Morales <daniel.cmorales3@gmail.com>
2021-04-27 09:27:33 -0500

    Fix path name to english

--- src/integrations/typescript/src/api/user/user.routes.ts
+++ src/integrations/typescript/src/api/user/user.routes.ts
@@ -7,15 +7,19 @@ export default class UserRoutes extends AbstractRoutes {
   constructor(
     private _userController = new UserController()
   ) {
-    super('/usuarios');
+    super('/user');
   }
 
+  /**
+   * These middlewares that you configure here
+   * will be applied just for that route
+   */
   protected routes: IRoute[] = [
     {
       path: '/',
       method: Methods.GET,
       handler: this._userController.getUsers,
-      localMiddleware: [checkMw]
+      middlewares: [checkMw]
     }
   ];
 
