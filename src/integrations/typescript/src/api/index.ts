abefd9454fc8e675a5bad5a4b77b05b8c70bc7ea
Daniel Morales <daniel.cmorales3@gmail.com>
2021-04-27 09:28:20 -0500

    Init comments

--- src/integrations/typescript/src/api/index.ts
+++ src/integrations/typescript/src/api/index.ts
@@ -4,6 +4,10 @@ import UserRoutes from './user/user.routes';
 import { checkMw } from '../middlewares/checkMw.middleware';
 
 export default class ApiRoutes extends AbstractApiRoutes {
+	/**
+	 * These middlewares that you configure here
+	 * will be applied for each component route
+	 */
 	protected configRoutes: IConfigRoute[] = [
 		{
 			routes: new UserRoutes(),
